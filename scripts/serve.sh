eea445ae89a450c031efe0ae586479edc639422f
Hans Tognon <learningway403@gmail.com>
2023-03-04 14:28:27 +0100

    utility update

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+WHITEBOARD_HOST=${STUDX_BASE_IP:-192.168.0.134}:8081
+
 docker compose up -d && \
-  caddy reverse-proxy --from "${WHITEBOARD_HOST:-192.168.0.134:8081}" \
-  --to "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' studx_whiteboard):8080"
+  caddy reverse-proxy --from "${WHITEBOARD_HOST:-0.0.0.0:8081}" \
+  --to "$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' studx_whiteboard):8080" \
   --change-host-header
